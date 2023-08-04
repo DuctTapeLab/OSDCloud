4f3e0305fe254fce33a19c00f5f849521574b0dc
DuctTapeLab <46982091+DuctTapeLab@users.noreply.github.com>
2023-08-04 12:33:52 -0500

    Add files via upload

---
+++ OSDCloudConfig.ps1
@@ -0,0 +1,22 @@
+﻿Write-Host  -ForegroundColor Yellow "Starting Custom OSDCloud Deployment ..."
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "================ OSD Cloud Deployment =================" -ForegroundColor Yellow
+Write-Host "=======================v.1.0.0 ========================" -ForegroundColor Yellow
+Write-Host "=======================================================" -ForegroundColor Yellow
+Write-Host "1: Windows 11 Enterprise 22H2"-ForegroundColor Yellow
+Write-Host "2: Exit`n"-ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+Import-Module OSD -Force
+Install-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-us -OSVersion 'Windows 11' -OSBuild 22H2 -Firmware -SkipAutopilot } 
+    '2' { Exit		}
+}
+
+wpeutil reboot
