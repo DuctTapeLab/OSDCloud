c9e17f49030eb046815fc3a675815a7f5515e88e
DuctTapeLab <46982091+DuctTapeLab@users.noreply.github.com>
2023-08-05 08:47:21 -0500

    Update OSDCloudConfig.ps1

--- OSDCloudConfig.ps1
+++ OSDCloudConfig.ps1
@@ -19,4 +19,17 @@ switch ($input)
     '2' { Exit		}
 }
 
+Write-Host "Cleaning up OSDCloud..." -ForegroundColor yellow 
+
+Copy-Item -Path C:\OSDCloud\Logs -Destination C:\Windows\Logs\OSDCloud -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\Drivers -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\OSDCloud\configs -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\OSDCloud\OS -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\OSDCloud\Drivers -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\OSDCloud\Packages -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\OSDCloud\Scripts -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\OSDCloud\Temp -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\OSDCloud\Logs -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path C:\OSDCloud -Force -Recurse -ErrorAction SilentlyContinue
+
 wpeutil reboot
